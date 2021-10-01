0ba2a0d07a3765b293d92c3204b931df58fa372b
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2021-10-02 01:48:00 +0600

    Homepage for non logged users

---
+++ pages/footer.php
@@ -0,0 +1,34 @@
+<div class='footer'>
+    <div class='copyright'>
+        &copy; 2022 Anovenus
+    </div>
+    <div class="footer-right">
+         <ul>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#features">Features</a>
+            </li>
+            <li>
+                <a href="#terms">Terms</a>
+            </li>
+            <li>
+                <a href="#privacy">Privacy</a>
+            </li>
+            <li>
+                <a href="#support">Support</a>
+            </li>
+            <li>
+                <div class='select-language'>
+                    <form action="select-language.php" method="post">
+                        <select>
+                            <option value="en">English</option>
+                            <option value="bn">বাংলা</option>
+                        </select>
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
