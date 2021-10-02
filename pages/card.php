4a2af7a7b1f1f70996656923e470972a4b15bd44
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2021-10-02 20:07:30 +0600

    Complete non-logged users homepage

---
+++ pages/card.php
@@ -0,0 +1,25 @@
+    <div class='card'>
+        <div class='card-header'>
+            <div class='card-info'>
+                <div class='name'>
+                    Ashiqur Rahman Alif
+                </div>
+                <div class='about'>
+                    Rajshahi * 2 hours ago
+                </div>
+            </div>
+            <div class='card-react'>
+                Love <br/>
+                53,254
+            </div>
+        </div>
+        <div class='card-content'>
+            <p>
+                Lorem Ipsum is simply dummy text
+                of the printing and typesetting 
+                industry. Lorem Ipsum has been the
+                industry's standard dummy text
+                ever since the 1500s.
+            </p>
+        </div>
+    </div>
